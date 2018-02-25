d67b12ec51fd4bad0e35901d72934f04f5a57a39
kmv10 <kmv10@duke.edu>
2018-02-25 12:41:46 -0500

    Added instruction generator

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,21 @@
 package controller;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
 
-public class Controller {
+import model.instructions.Instruction;
+
+public class Controller{
+    Queue<Instruction> instructionQueue;
+    
+    public Controller() {
+	instructionQueue = new PriorityQueue<>();
+    }
+    
+    public Instruction getInstruction() {
+	Instruction i = instructionQueue.peek();
+	instructionQueue.remove();
+	return i;
+    }
 
 }
--- src/model/commands/Command.java
+++ src/model/commands/Command.java
@@ -1,4 +1,4 @@
-package command;
+package model.commands;
 
 import java.util.List;
 
