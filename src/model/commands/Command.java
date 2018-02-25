10d140027c388d604d128c9cd742c2cd150358c5
Martin <mm617@duke.edu>
2018-02-25 14:28:20 -0500

    changed command design for instructions

--- src/model/commands/Command.java
+++ src/model/commands/Command.java
@@ -8,13 +8,15 @@ import model.instructions.Instruction;
 public abstract class Command {
 	public List<Command> commands;
 	public List<Double> parameters;
+	public VarDict vars;
+	public CommandDict custom;
 	
 	public Command() {
 		commands = new ArrayList<Command>();
 		parameters = new ArrayList<Double>();
 	}
 	
-	public abstract List<Instruction> execute() throws CommandException;
+	public abstract List<States> execute(State s) throws CommandException;
 	
 	public abstract double getReturnValue();
 	
@@ -23,4 +25,9 @@ public abstract class Command {
 	public void clearParameters() {
 		parameters.clear();
 	}
+	
+	public setDictionaries(Vardict, commanddirc) {
+		
+	}
+	
 }
--- src/model/commands/Move.java
+++ src/model/commands/Move.java
@@ -2,23 +2,30 @@ package model.commands;
 
 import java.util.List;
 
+import com.sun.org.apache.xerces.internal.utils.XMLSecurityManager.State;
+
 import model.instructions.Instruction;
 import model.instructions.MoveInstruction;
 
+import java.awt.event.ActionEvent;
 import java.util.LinkedList;
 
 public class Move extends Command{
 
 	@Override
-	public List<Instruction> execute() throws CommandException{
+	public List<State> execute(State s) throws CommandException{
 		clearParameters();
 		List<Instruction> instructions = new LinkedList<Instruction>();
 		instructions.addAll(commands.get(0).execute());
 		parameters.add(commands.get(0).getReturnValue());
 		validate();
-		Instruction move = new MoveInstruction();
+		/*Instruction move = new MoveInstruction();
 		move.param = parameters.get(0);
 		instructions.add(move);
+		*/
+		//ActionEvent move = new ActionEvent(this, move.ACTION_PERFORMED, "move 4");
+		//move.
+		
 		return instructions;
 	}
 
