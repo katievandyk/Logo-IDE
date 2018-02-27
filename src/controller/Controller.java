6b8f438dfc469b3dd1520cabfa760036145aaa86
Brandon Dalla Rosa <brandon.dalla.rosa@duke.edu>
2018-02-27 12:36:27 -0500

    Added minor fixes to code

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -19,46 +19,46 @@ import model.parser.Parser;
  *
  */
 public class Controller{
-    private Parser Parser;
-    private State lastState; 
-    private ViewController ViewController;
+	private Parser Parser;
+	private State lastState; 
+	private ViewController ViewController;
 
 
-    public Controller() {
-	Parser = new Parser();
-	lastState = new State();
-	ViewController = new ViewController();
+	public Controller() {
+		Parser = new Parser();
+		lastState = new State();
+		ViewController = new ViewController();
 
-    }
-
-    public void initialize(Stage primaryStage) {
-	ViewController.initialize(primaryStage, this);
-    }
-    
-    private void sendError(String message) {
-    	ViewController.sendError(message);
-    }
-    
-    public void update(String currentInput) {
-	LinkedList<Command> commands = (LinkedList<Command>) Parser.getCommands(currentInput);
-	if(commands != null) {
-		LinkedList<State> states = new LinkedList<>();
-		for(Command c : commands) {
-		    try {
-			states.addAll(c.execute(lastState));
-		    } catch (CommandException e) {
-			System.out.println("HERE");
-			String error = "Wrong input";
-			sendError(error);
-		    }
-		    lastState = states.getLast();
-		    ViewController.updateTurtle(states); 
-		} 
-	}
-	else {
-	    sendError("Invalid command");
 	}
 
-    }
+	public void initialize(Stage primaryStage) {
+		ViewController.initialize(primaryStage, this);
+	}
+
+	private void sendError(String message) {
+		ViewController.sendError(message);
+	}
+
+	public void update(String currentInput) {
+		LinkedList<Command> commands = (LinkedList<Command>) Parser.getCommands(currentInput);
+		if(commands != null) {
+			LinkedList<State> states = new LinkedList<>();
+			for(Command c : commands) {
+				try {
+					states.addAll(c.execute(lastState));
+				} catch (CommandException e) {
+					System.out.println("HERE");
+					String error = "Wrong input";
+					sendError(error);
+				}
+				lastState = states.getLast();
+				ViewController.updateTurtle(states); 
+			} 
+		}
+		else {
+			sendError("Invalid command");
+		}
+
+	}
 
 }
