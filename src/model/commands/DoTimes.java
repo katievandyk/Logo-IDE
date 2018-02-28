3362e85f79416c29e3094a5d4849e988cd56f32f
Martin <mm617@duke.edu>
2018-02-28 01:45:30 -0500

    added remaining commands

--- src/model/commands/DoTimes.java
+++ src/model/commands/DoTimes.java
@@ -4,23 +4,30 @@ import java.util.List;
 
 import model.state.State;
 
-public class Repeat extends Command {
+public class DoTimes extends Command {
 
 	public double returnval;
 
 	@Override
 	public List<State> execute(List<State> states) throws CommandException {
-		states = (commands.get(0).get(0).execute(states));
-		parameters.add(commands.get(0).get(0).getReturnValue());
+		String varName = null;
+		try {
+			varName = ((StringVar) commands.get(0).get(0)).getString();
+		}
+		catch(Exception e) {
+			throw new CommandException("Syntax error: variable expected");
+		}
+		
+		states = (commands.get(0).get(1).execute(states));
+		parameters.add(commands.get(0).get(1).getReturnValue());
 		
 		returnval = 0;
-		for (int i = 0; i < parameters.get(0); i++) {
-			variableDictionary.addVariable(":repcount", (double) i+1);
+		for (int i = 1; i <= parameters.get(0); i++) {
+			variableDictionary.addVariable(varName, (double) i);
 			for (Command c :commands.get(1)) {
 				states = c.execute(states);
 				returnval = c.getReturnValue();
-			}
-			
+			}	
 		}
 		validate();
 		return states;
--- src/model/commands/For.java
+++ src/model/commands/For.java
@@ -4,23 +4,33 @@ import java.util.List;
 
 import model.state.State;
 
-public class Repeat extends Command {
+public class For extends Command {
 
 	public double returnval;
 
 	@Override
 	public List<State> execute(List<State> states) throws CommandException {
-		states = (commands.get(0).get(0).execute(states));
-		parameters.add(commands.get(0).get(0).getReturnValue());
+		String varName = null;
+		try {
+			varName = ((StringVar) commands.get(0).get(0)).getString();
+		}
+		catch(Exception e) {
+			throw new CommandException("Syntax error in For");
+		}
+		
+		for (int i = 1; i <= 3; i++) {
+			states = (commands.get(0).get(i).execute(states));
+			parameters.add(commands.get(0).get(i).getReturnValue());
+		}
+		
 		
 		returnval = 0;
-		for (int i = 0; i < parameters.get(0); i++) {
-			variableDictionary.addVariable(":repcount", (double) i+1);
+		for (double i = parameters.get(1); i <= parameters.get(2); i += parameters.get(3)) {
+			variableDictionary.addVariable(varName, (double) i);
 			for (Command c :commands.get(1)) {
 				states = c.execute(states);
 				returnval = c.getReturnValue();
-			}
-			
+			}	
 		}
 		validate();
 		return states;
@@ -33,9 +43,6 @@ public class Repeat extends Command {
 
 	@Override
 	public void validate() throws CommandException {
-		if (parameters.size() != 1) {
-			throw new CommandException("Invalid number of arguments: " + parameters.size());
-		}
 		else if (parameters.get(0) <= 0) {
 			throw new CommandException("Negative argument given");
 		}
