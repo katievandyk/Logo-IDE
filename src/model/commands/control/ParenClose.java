f0f7b8130cb3c6a171040bb781c38c19fc9a8ace
Martin <mm617@duke.edu>
2018-03-04 22:51:52 -0500

    Added parentheses commands

---
+++ src/model/commands/control/ParenClose.java
@@ -0,0 +1,25 @@
+package model.commands.control;
+
+import java.util.List;
+
+import model.commands.Command;
+import model.commands.CommandException;
+import model.state.State;
+
+public class ParenClose extends Command {
+
+	@Override
+	public List<State> execute(List<State> states) throws CommandException {
+		return states;
+	}
+
+	@Override
+	public double getReturnValue() {
+		return 0;
+	}
+
+	@Override
+	protected void validate() throws CommandException {
+	}
+
+}
