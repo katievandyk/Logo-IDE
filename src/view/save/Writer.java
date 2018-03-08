27012fbdb305bdfab6917cfa8844ecdc474a160e
kmv10 <kmv10@duke.edu>
2018-03-08 16:25:50 -0500

    Pause

---
+++ src/view/save/Writer.java
@@ -0,0 +1,18 @@
+package view.save;
+
+import java.util.ResourceBundle;
+
+import view.ViewController;
+
+public class Writer {
+    
+    
+    private final ResourceBundle COLOR_RESOURCES = ResourceBundle.getBundle("resources/settings/workspace");
+    
+    public Writer(ViewController vc) {
+	
+    }
+    
+    
+
+}
