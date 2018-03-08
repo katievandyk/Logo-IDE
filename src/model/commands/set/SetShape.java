ab9928c676e68d02c5bdd3546b1c5eb894912274
Martin <mm617@duke.edu>
2018-03-07 20:48:18 -0500

    Added new set command classes

---
+++ src/model/commands/set/SetShape.java
@@ -0,0 +1,26 @@
+package model.commands.set;
+
+import model.commands.CommandException;
+import model.state.State;
+
+public class SetShape extends Set {
+
+	@Override
+	protected State setNextState(State nextState) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getReturnValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void validate() throws CommandException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
