a572afa8d98d9e56bed82cbf39c3e39a927d97ce
Martin <mm617@duke.edu>
2018-03-08 17:29:34 -0500

    refactored if and ifelse

--- src/model/commands/control/IfElse.java
+++ src/model/commands/control/IfElse.java
@@ -6,32 +6,26 @@ import model.commands.Command;
 import model.commands.CommandException;
 import model.state.State;
 
-public class IfElse extends Command {
-
-	private double returnval;
+public class IfElse extends Conditional {
 	
 	@Override
 	public List<State> execute(List<State> states) throws CommandException {
-		states = commands.get(0).execute(states);
-		if (commands.get(0).getReturnValue() != 0) {
-			states = commands.get(1).execute(states);
-			returnval = commands.get(1).getReturnValue();
-		}
-		else {
+		states = super.execute(states);
+		if (runElse) {
 			states = commands.get(2).execute(states);
 			returnval = commands.get(2).getReturnValue();
 		}
-		validate();
 		return states;
 	}
 
 	@Override
-	public double getReturnValue() {
-		return returnval;
-	}
-
-	@Override
 	protected void validate() throws CommandException {
+		if (!(commands.get(1) instanceof ListOpen)) {
+			throw new CommandException("List input expected in second argument of ifelse");
+		}
+		else if (!(commands.get(2) instanceof ListOpen)) {
+			throw new CommandException("List input expected in third argument of ifelse");
+		}
 	}
 
 }
