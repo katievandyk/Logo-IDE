7fb1bb7c06d63621ad54d9cd0251dc8ead31496f
Martin Muenster <mm617@duke.edu>
2018-05-04 15:24:24 -0400

    added new classes

---
+++ src/model/commands/control/Stamp.java
@@ -0,0 +1,21 @@
+package model.commands.control;
+
+import java.util.List;
+
+import model.commands.Command;
+import model.commands.CommandException;
+import model.state.State;
+
+public class Stamp extends Command {
+
+	@Override
+	public List<State> execute(List<State> states) throws CommandException {
+		return states;
+	}
+
+	@Override
+	public double getReturnValue() {
+		return 0;
+	}
+
+}
